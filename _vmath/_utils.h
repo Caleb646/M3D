ef61b23b0a0accc6f62bab9510149e6ecfc6afdc
Caleb <calebthomas646@yahoo.com>
2021-11-14 16:34:08 -0600

    Starting switching to a header only library.

--- _vmath/_utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef UTILS_H
-#define UTILS_H
-
-float degreesToRadians(float degrees);
-
-#endif
--- src/Utils.h
+++ src/Utils.h
@@ -1,8 +1,8 @@
-#include "_utils.h"
+#pragma once
 
 constexpr auto M_PI = 3.14159265358979323846264338327950288;
 
-float degreesToRadians(float degrees)
+inline float degreesToRadians(float degrees)
 {
 	return (degrees * (M_PI / 180.0f));
 }
